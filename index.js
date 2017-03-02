9c3e95e79273b533bdb14ca3c814c044ee1cbfea
rsstdd <rosstod@gmail.com>
2017-03-02 09:58:38 -0800

    Refactored code

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
 #! /usr/bin/env node
 
-console.log('This is the filesearch script');
+console.log('Brought To you by the Filesearch Script:');
 
-console.log(process.argv);
 const userArgs = process.argv.slice(2);
 const searchPattern = userArgs[0];
-
-console.log('userArgs:', userArgs, 'searchPattern:', searchPattern);
 
 const exec = require('child_process').exec;
 const child = exec(`ls -a | grep ${searchPattern}`, (err, stdout, stderr) => {
