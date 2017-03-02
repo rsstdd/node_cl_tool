b42d19646d9d7107865380cea79e258d59dada36
rsstdd <rosstod@gmail.com>
2017-03-02 09:53:46 -0800

    Functionality complete

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ const userArgs = process.argv.slice(2);
 const searchPattern = userArgs[0];
 
 console.log('userArgs:', userArgs, 'searchPattern:', searchPattern);
+
+const exec = require('child_process').exec;
+const child = exec(`ls -a | grep ${searchPattern}`, (err, stdout, stderr) => {
+  console.log(stdout);
+})
