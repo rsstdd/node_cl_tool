70a8f36ddec22a58cbe9130a5f193c5832094ab6
rsstdd <rosstod@gmail.com>
2017-03-02 09:48:51 -0800

    Grab user Args from the CL

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 #! /usr/bin/env node
 
 console.log('This is the filesearch script');
+
+console.log(process.argv);
+const userArgs = process.argv.slice(2);
+const searchPattern = userArgs[0];
+
+console.log('userArgs:', userArgs, 'searchPattern:', searchPattern);
